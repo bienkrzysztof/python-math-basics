05859e54241c6e74996a255659735c4e63fc513b
Krzysztof Bień <bien.krzysztoff@gmail.com>
2026-08-18 17:19:36 +0200

    Add Python math basics

---
+++ math_basics.py
@@ -0,0 +1,24 @@
+def norm(point):
+    square_sum = 0
+    for x in point:
+        square_sum += x ** 2
+    return square_sum ** 0.5
+
+
+def mean(x):
+    return sum(x) / len(x)
+
+
+def variance(x):
+    m = mean(x)
+    return sum((value - m) ** 2 for value in x) / len(x)
+
+
+def is_prime(n):
+    a = n // 2 + 1
+    if n < 2:
+        return False
+    for w in range(2, a):
+        if n % w == 0:
+            return False
+    return True
